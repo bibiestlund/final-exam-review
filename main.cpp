ef8e2a00ad8fc5ab101922eb8f5be084a4a92e97
bibiestlund <bibi.estlund@gmail.com>
2017-05-05 00:39:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
-//A program that helps students review for the C++ final exam.
+//
+//  main.cpp
+//  final-exam-review
+//
+//  Created by Kartapurkh Estlund on 5/4/17.
+//  Copyright © 2017 Bibi Khalsa Estlund. All rights reserved.
+//
+//  A program to help students of CSIS 123 review for the final exam.
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
+void print_question(string[], int&);
+
 int main()
+
 {
-
-
-
-
-
-return 0;
+    string question[]= {0};
+    int i;
+    
+    print_question(question, i);
+    
+        
+    return 0;
+}
+void print_question(string question[], int& i)
+{
+    for(i=1; i < 100; i++)
+    {
+        cout << question[i];
+    }
+   
 }
